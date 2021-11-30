fe5af8fc417a50e70e96a5580fe8d7062047a36c
FU, Chaojie <fuchaojieme@outlook.com>
2021-11-30 22:55:11 +0800

    2021-11-30

--- PersonnelManagementSystem.cpp
+++ PersonnelManagementSystem.cpp
@@ -2,40 +2,107 @@
 
 bool PersonnelManagementSystem::IsEmpty()
 {
+    return (UndergraduateStudent_List.empty() || FullTimePostgraduate_List.empty() || OnJobPostgraduate_List.empty() || Staff_List.empty() || Teacher_List.empty());
 }
 
 size_t PersonnelManagementSystem::NumOfMembers()
 {
-}
-
-void PersonnelManagementSystem::InsertMemberAtEnd(union Member &m_Member)
-{
-}
-
-void PersonnelManagementSystem::InsertMemberAtFront(union Member &m_Member)
-{
-}
-
-void PersonnelManagementSystem::DeleteMemberByName(Name name)
-{
+    return (UndergraduateStudent_List.size() + FullTimePostgraduate_List.size() + OnJobPostgraduate_List.size() + Staff_List.size() + Teacher_List.size());
 }
 
 void PersonnelManagementSystem::DeleteMemberByIDNumber(IDNumber idnumber)
 {
-}
+    std::list<UndergraduateStudent>::iterator UndergraduateStudent_Iter = UndergraduateStudent_List.begin();
+    for (; UndergraduateStudent_Iter != UndergraduateStudent_List.end(); UndergraduateStudent_Iter++)
+    {
+        if (UndergraduateStudent_Iter->getIDNumber() == idnumber)
+        {
+            UndergraduateStudent_List.erase(UndergraduateStudent_Iter);
+        }
+    }
 
-void PersonnelManagementSystem::LookupByName(Name name)
-{
+    std::list<FullTimePostgraduate>::iterator FullTimePostgraduate_Iter = FullTimePostgraduate_List.begin();
+    for (; FullTimePostgraduate_Iter != FullTimePostgraduate_List.end(); FullTimePostgraduate_Iter++)
+    {
+        if (FullTimePostgraduate_Iter->getIDNumber() == idnumber)
+        {
+            FullTimePostgraduate_List.erase(FullTimePostgraduate_Iter);
+        }
+    }
+
+    std::list<OnJobPostgraduate>::iterator OnJobPostgraduate_Iter = OnJobPostgraduate_List.begin();
+    for (; OnJobPostgraduate_Iter != OnJobPostgraduate_List.end(); OnJobPostgraduate_Iter++)
+    {
+        if (OnJobPostgraduate_Iter->getIDNumber() == idnumber)
+        {
+            OnJobPostgraduate_List.erase(OnJobPostgraduate_Iter);
+        }
+    }
+
+    std::list<Staff>::iterator Staff_Iter = Staff_List.begin();
+    for (; Staff_Iter != Staff_List.end(); Staff_Iter++)
+    {
+        if (Staff_Iter->getIDNumber() == idnumber)
+        {
+            Staff_List.erase(Staff_Iter);
+        }
+    }
+
+    std::list<Teacher>::iterator Teacher_Iter = Teacher_List.begin();
+    for (; Teacher_Iter != Teacher_List.end(); Teacher_Iter++)
+    {
+        if (Teacher_Iter->getIDNumber() == idnumber)
+        {
+            Teacher_List.erase(Teacher_Iter);
+        }
+    }
 }
 
 void PersonnelManagementSystem::LookupByIDNumber(IDNumber idnumber)
 {
-}
+    std::cout << "Member with ID Number" << idnumber << "is: " << std::endl;
+    std::list<UndergraduateStudent>::iterator UndergraduateStudent_Iter = UndergraduateStudent_List.begin();
+    for (; UndergraduateStudent_Iter != UndergraduateStudent_List.end(); UndergraduateStudent_Iter++)
+    {
+        if (UndergraduateStudent_Iter->getIDNumber() == idnumber)
+        {
+            UndergraduateStudent_Iter->printInfo();
+        }
+    }
 
-void PersonnelManagementSystem::PrintMemberInfoByName(Name name)
-{
-}
+    std::list<FullTimePostgraduate>::iterator FullTimePostgraduate_Iter = FullTimePostgraduate_List.begin();
+    for (; FullTimePostgraduate_Iter != FullTimePostgraduate_List.end(); FullTimePostgraduate_Iter++)
+    {
+        if (FullTimePostgraduate_Iter->getIDNumber() == idnumber)
+        {
+            FullTimePostgraduate_Iter->printInfo();
+        }
+    }
 
-void PersonnelManagementSystem::PrintAllInfos()
-{
+    std::list<OnJobPostgraduate>::iterator OnJobPostgraduate_Iter = OnJobPostgraduate_List.begin();
+    for (; OnJobPostgraduate_Iter != OnJobPostgraduate_List.end(); OnJobPostgraduate_Iter++)
+    {
+        if (OnJobPostgraduate_Iter->getIDNumber() == idnumber)
+        {
+            OnJobPostgraduate_Iter->printInfo();
+        }
+    }
+
+    std::list<Staff>::iterator Staff_Iter = Staff_List.begin();
+    for (; Staff_Iter != Staff_List.end(); Staff_Iter++)
+    {
+        if (Staff_Iter->getIDNumber() == idnumber)
+        {
+            Staff_Iter->printInfo();
+        }
+    }
+
+    std::list<Teacher>::iterator Teacher_Iter = Teacher_List.begin();
+    for (; Teacher_Iter != Teacher_List.end(); Teacher_Iter++)
+    {
+        if (Teacher_Iter->getIDNumber() == idnumber)
+        {
+            Teacher_Iter->printInfo();
+        }
+    }
 }
--- PersonnelManagementSystem.h
+++ PersonnelManagementSystem.h
@@ -11,18 +11,32 @@ class PersonnelManagementSystem
         std::list<OnJobPostgraduate> OnJobPostgraduate_List;
         std::list<Staff> Staff_List;
         std::list<Teacher> Teacher_List;
+        bool _checkNewMemberByIDNumber(UndergraduateStudent & m_Member);
+        bool _checkNewMemberByIDNumber(FullTimePostgraduate & m_Member);
+        bool _checkNewMemberByIDNumber(OnJobPostgraduate & m_Member);
+        bool _checkNewMemberByIDNumber(Staff & m_Member);
+        bool _checkNewMemberByIDNumber(Teacher & m_Member);
     public:
         bool IsEmpty();
         size_t NumOfMembers();
-        void InsertMemberAtEnd(union Member & m_Member);
-        void InsertMemberAtFront(union Member & m_Member);
+        void InsertMemberAtEnd(UndergraduateStudent & m_Member);
+        void InsertMemberAtEnd(FullTimePostgraduate & m_Member);
+        void InsertMemberAtEnd(OnJobPostgraduate & m_Member);
+        void InsertMemberAtEnd(Staff & m_Member);
+        void InsertMemberAtEnd(Teacher & m_Member);
 
-        void DeleteMemberByName(Name name);
+        void InsertMemberAtFront(UndergraduateStudent & m_Member);
+        void InsertMemberAtFront(FullTimePostgraduate & m_Member);
+        void InsertMemberAtFront(OnJobPostgraduate & m_Member);
+        void InsertMemberAtFront(Staff & m_Member);
+        void InsertMemberAtFront(Teacher & m_Member);
+
         void DeleteMemberByIDNumber(IDNumber idnumber); 
 
-        void LookupByName(Name name);
         void LookupByIDNumber(IDNumber idnumber);
 
         void PrintMemberInfoByName(Name name);
+        void PrintMemberInfoByIDNumber(IDNumber idnumber);
+
         void PrintAllInfos();
 };
